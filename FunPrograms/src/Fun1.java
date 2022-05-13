8af4ecbde85f3c2cd9cce4f0772d9c28af45b41b
acer <acer@DESKTOP-674KRTC>
2022-05-13 16:07:42 +0530

    another statement added

---
+++ FunPrograms/src/Fun1.java
@@ -0,0 +1,14 @@
+
+public class Fun1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("fun");
+		System.out.println("more fun");
+		System.out.println("less fun");
+
+
+		
+	}
+
+}
